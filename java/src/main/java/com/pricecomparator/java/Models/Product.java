a6a0f432758bf1680ce515750c530f8ab2131e29
arubla <arunblatchley@gmail.com>
2023-01-22 16:40:51 -0500

    setting up classes

---
+++ java/src/main/java/com/pricecomparator/java/Models/Product.java
@@ -0,0 +1,13 @@
+package com.pricecomparator.java.Models;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private String name = "";
+    private String price = ""; //this is a string because it is only going to be read by the user.
+    private String url = "";
+    private String description = "";
+
+
+}
---
+++ java/src/main/java/com/pricecomparator/java/Models/ProductResults.java
@@ -0,0 +1,11 @@
+package com.pricecomparator.java.Models;
+
+import java.util.List;
+
+public class ProductResults {
+
+    private String query = "";
+    private List<Product> results = null;
+
+
+}
